de36f8c9fe4c64d6bf1854e6431a9dad579f66bc
Fu Thong <kibble.bytes08@gmail.com>
2020-03-01 16:09:33 -0500

    Add build folder

---
+++ build/precache-manifest.010f9d3b1e63afd9ecf95b83feec3808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a56ad5fc07581747a1495d761eb43dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b61b9228818c8e9087d",
+    "url": "/static/css/main.5b83ee3e.chunk.css"
+  },
+  {
+    "revision": "d067e4bb5cb96b630c38",
+    "url": "/static/js/2.5c5bc2e4.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.5c5bc2e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b61b9228818c8e9087d",
+    "url": "/static/js/main.e484c819.chunk.js"
+  },
+  {
+    "revision": "85e456f32443e2310da8",
+    "url": "/static/js/runtime-main.06f7d365.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
