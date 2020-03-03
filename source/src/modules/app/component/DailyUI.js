fa6fe5f677498ff66ccb15d92ae9cd07fe4ba1ce
Fu Thong <kibble.bytes08@gmail.com>
2020-03-03 16:59:03 -0500

    Add header and body text to dailyui page.

--- source/src/modules/app/component/DailyUI.js
+++ source/src/modules/app/component/DailyUI.js
@@ -4,23 +4,31 @@ import Design from './Design';
 import ChallengeHeader from './ChallengeHeader';
 import {Basic, Colored} from '../dailyui/days/Day1';
 import {Empty, HalfHour, Hour1, Hour2, Hour3} from '../dailyui/days/Day2';
+import '../../../App.scss';
 
 const DailyUI = () => {
-    return <div
-        className="app-content"
-        style={{width: '1200px', margin: 'auto'}}
-    >
+    return <div style={{width: '1200px', margin: 'auto'}}>
+        <PageTitle>DailyUI Challenges</PageTitle>
+        <Typography>
+            <a href="https://www.dailyui.co/" style={{fontWeight: 'bold', textDecoration: 'underline'}}>DailyUI</a> has a 100-day design challenge, with a different prompt each day.
+            Since I am a programmer dabbling in design rather than a designer dabbling in code, I'm tackling this a little differently.
+            Instead of a mockup per day, I'll iterate on a semi-functional, interactive prototype of each daily prompt.
+        </Typography>
+        <Typography>
+            Hopefully as time goes by, I'll be putting out higher-quality or more featureful prototypes within the time constraints.
+            Perhaps by the end of it all, I'll end up with a half-decent UI component library.
+        </Typography>
         <Grid style={{width: '100%'}}>
             <Cell size={12}>
                 <ChallengeHeader>001> Sign Up</ChallengeHeader>
             </Cell>
             <Cell size={4}>
-                <Design title="Basics">
+                <Design title="1 Hour">
                     <Basic />
                 </Design>
             </Cell>
             <Cell size={4}>
-                <Design title="Colored">
+                <Design title="90 Minutes">
                     <Colored />
                 </Design>
             </Cell>
@@ -54,5 +62,22 @@ const Daily = ({title, Half, First, Second, Third}) => {
         </Cell>
     </>;
 }
+
+const PageTitle = ({children}) => (
+    <div style={{padding: '16px'}}>
+        <span style={{fontFamily: 'Oxanium', fontSize: '56px'}}>
+            {children}
+        </span>
+    </div>
+);
+
+const Typography = ({children}) => (
+    <div style={{textAlign: 'left', margin: '40px auto', width: '1200px'}}>
+        <span style={{
+            fontFamily: 'Merriweather', fontSize: '24px'}}>
+            {children}
+        </span>
+    </div>
+);
 
 export default DailyUI;
